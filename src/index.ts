4c8bb941dbc6339c55c2d9b63cab327dd4be1337
NVS <nvskiev@gmail.com>
2022-12-11 20:51:06 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement
+const ctx = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Char {
+    constructor() {
+        
+    }
+}
+
+class Effect {
+    constructor() {
+        
+    }
+}
